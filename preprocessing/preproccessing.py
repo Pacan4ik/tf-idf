079146fb51d8666dbc4fdb84424b440f2ef77c7a
Денис Галин <Blinkdash>
2024-06-20 14:58:13 +0300

    Андре	й

--- preprocessing/preproccessing.py
+++ preprocessing/preproccessing.py
@@ -1,15 +1,27 @@
 from nltk.tokenize import word_tokenize
 import re
+import nltk
 import string
 import spacy
-from spacy.lang.ru.examples import sentences
 import os
-from pdfminer.high_level import extract_pages, extract_text
+from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
+
+
+# Класс для представления статей
+class Article:
+    def __init__(self, file_name, file_text):
+        self.file_name = file_name
+        self.file_text = file_text
+
+    def __repr__(self):
+        return f"Статья {self.file_name}, текст статьи: {self.file_text}"
+
 
 # Загрузка стоп-слов и пунктуации
 with open('data/stopwords_all.txt', 'r', encoding='utf-8') as file1:
     stop_words = set(file1.read().splitlines())
+nltk.download('punkt')
 punctuation = string.punctuation
 # Загрузка языковой модели spaCy
 nlp = spacy.load("ru_core_news_sm")
@@ -21,19 +33,17 @@ def text_extraction(path):
 
     for filename in os.scandir(path):
         if filename.is_file():
-            extracted_text_array.append('')
+            extracted_text_array.append(Article(filename.name, ''))
 
             for pagenum, page in enumerate(extract_pages(filename.path)):
                 for element in page:
                     if isinstance(element, LTTextContainer):
-                        s = element.get_text().replace('\n', '').lower().replace('ё', 'е')
-                        if (s[-1] != '-'):
-                            extracted_text_array[c] += str(s + " ")
-                        else:
-                            extracted_text_array[c] += str(s)
-        extracted_text_array[c] = re.sub(r'[\d!\"#$%&\'()*+,./:;<=>?@\[\]^_`{|}~]', ' ', extracted_text_array[c])
+                        s = element.get_text().replace('-\n', '').replace('\n', '').lower().replace('ё', 'е')
+                        extracted_text_array[c].file_text += str(s + " ")
 
-        extracted_text_array[c] = lemmatize(delete_stop_words(extracted_text_array[c]))
+        extracted_text_array[c].file_text = re.sub(r'[\d!\"#$%&\'()*+,./:;<=>?@\[\]^_`{|}~]', ' ',
+                                                   extracted_text_array[c].file_text)
+        extracted_text_array[c].file_text = lemmatize(delete_stop_words(extracted_text_array[c].file_text))
 
         c += 1
 
