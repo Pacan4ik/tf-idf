250fab425783dbee3b46da9e131f619e9cb8cb91
Pacan4ik <andreyka.potekhin@mail.ru>
2024-06-20 19:39:29 +0300

    command line interface

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
+from preprocessing import preproccessing
+from tfidfstat import tfidf
+from prettyprinter import md
+from prettyprinter import excel_print
+import argparse as ap
+import os
+
+
+def parse_arguments():
+    parser = ap.ArgumentParser(description='Выделяет частоту термина для масиива файлов.')
+    parser.add_argument('--source', type=str, required=True,
+                        help='Путь к директории с данными.')
+    parser.add_argument('--out_format', type=str, default='console', choices=['console', 'csv', 'xlsx'],
+                        help='Формат вывода: console, csv, xlsx.')
+    parser.add_argument('--output', type=str,
+                        help='Путь для сохранения результатов (обязательно для форматов csv и xlsx).')
+    parser.add_argument('--threshold', type=float, default=0.005,
+                        help='Пороговое значение для фильтрации TF-IDF значений.')
+
+    arguments = parser.parse_args()
+    if arguments.out_format in ['csv', 'xlsx'] and not arguments.output:
+        parser.error("--output обязателен при выборе форматов вывода csv или xlsx.")
+    return arguments
+
+
+def valid_path(path):
+    if not os.path.exists(path):
+        raise ap.ArgumentTypeError(f"Путь '{path}' не существует.")
+    return path
+
 
 if __name__ == '__main__':
-    print('dfgvbjk; jkerg bs')
+    args = parse_arguments()
+    valid_path(args.source)
+
+    docs = preproccessing.text_extraction(args.source)
+    try:
+        res = tfidf.tfidf(docs)
+    except UserWarning as e:
+        print('Директория --source должна содержать минимум 2 файла форматов pdf или word.')
+        raise SystemExit(e)
+
+    file_name_stat = [(article.file_name, tfidf_dict) for article, tfidf_dict in res]
+
+    if args.out_format == 'console':
+        md.print_table(file_name_stat, threshold=args.threshold)
+    if args.out_format == 'csv':
+        excel_print.save_csv(file_name_stat, args.output, threshold=args.threshold)
+    if args.out_format == 'xlsx':
+        excel_print.save_xlsx(file_name_stat, args.output, threshold=args.threshold)
