ca6655ac3a445168008ea65863eff28385c55fb6
Денис Галин <Blinkdash>
2024-06-21 16:35:28 +0300

    Привет

--- preprocessing/preproccessing.py
+++ preprocessing/preproccessing.py
@@ -1,10 +1,10 @@
 import nltk
 from nltk.tokenize import word_tokenize
 import re
-import nltk
 import string
 import spacy
 import os
+import docx
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 
@@ -35,16 +35,23 @@ def text_extraction(path):
     for filename in os.scandir(path):
         if filename.is_file():
             extracted_text_array.append(Article(filename.name, ''))
+            if filename.name.endswith('.docx'):
+                doc = docx.Document(filename.path)
+                for paragraph in doc.paragraphs:
+                    s = paragraph.text.replace('-\n', '').replace('\n', '').lower().replace('ё', 'е')
+                    extracted_text_array[c].file_text += str(s + " ")
+            else:
 
-            for pagenum, page in enumerate(extract_pages(filename.path)):
-                for element in page:
-                    if isinstance(element, LTTextContainer):
-                        s = element.get_text().replace('-\n', '').replace('\n', '').lower().replace('ё', 'е')
-                        extracted_text_array[c].file_text += str(s + " ")
+                for pagenum, page in enumerate(extract_pages(filename.path)):
+                    for element in page:
+                        if isinstance(element, LTTextContainer):
+                            s = element.get_text().replace('-\n', '').replace('\n', '').lower().replace('ё', 'е')
+                            extracted_text_array[c].file_text += str(s + " ")
 
         extracted_text_array[c].file_text = re.sub(r'[\d!\"#$%&\'()*+,./:;<=>?@\[\]^_`{|}~]', ' ',
                                                    extracted_text_array[c].file_text)
         extracted_text_array[c].file_text = lemmatize(delete_stop_words(extracted_text_array[c].file_text))
+        extracted_text_array[c].file_name = extracted_text_array[c].file_name.replace('.pdf', '').replace('.docx', '')
 
         c += 1
 
